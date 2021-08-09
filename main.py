529deeb66d7923b425e6a774bf9cc706e6c1dc61
RowanAshraff <88684837+RowanAshraff@users.noreply.github.com>
2021-08-09 22:21:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,72 +1,51 @@
-# Click on any colour in the video to subtract it from the video, you can change the VarThreshold, history, and Kernel as you please
-## Havee fun!!
-
+# Choose 2 points in an image to extract the foreground
+# Have fun!!
 import cv2
 import numpy as np
-fgbg = []
 
 
-def trackbar_callback(x):
-    global fgbg
-    fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=cv2.getTrackbarPos('VarThreshold', 'img'), history=cv2.getTrackbarPos('History', 'img'))
+refPt = []
+new = []
 
 
-def backgroundSubtraction(frame, fgbg):
-    fmask = fgbg.apply(frame)
-    res = cv2.bitwise_and(frame, frame, mask=fmask)
-    return res
+def foregroundExtraction(img):
+    mask = np.zeros(img.shape[:2], np.uint8)    # mask with dimensions (rows, cols)
+    print(img.shape)
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    # (x,y, cols, rows)
+    rect = (min(refPt[0][0], refPt[1][0]), min(refPt[0][1], refPt[1][1]), abs(refPt[0][0] - refPt[1][0]), abs(refPt[0][1] - refPt[1][1]))       # try to automate this part with mouse events
+
+    # this modifies mask with 0,1,2,3 and the 5 param is the no. of iterations
+    cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 8, cv2.GC_INIT_WITH_RECT)
+    # 0,2 are foreground, 1,3 are background
+    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+    img = img * mask2[:, :, np.newaxis]
+    return img
 
 
-def morphologicalTransformations(m, frame, kernel):
-    # case 'w'
-    if m == 1:
-        image = cv2.erode(frame, kernel, iterations=1)
-    # case 'x'
-    elif m == 2:
-        image = cv2.dilate(frame, kernel, iterations=1)
-    # case 'y'
-    elif m == 3:
-        image = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
-    # case 'z'
-    elif m == 4:
-        image = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernel)
-    else:
-        image = frame
-    return image
+def click_event(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Put the clicks coordinates in a variable
+        print(1)
+        refPt.append([x, y])
+        if len(refPt) >= 2:
+            new.append([x, y])
+            if len(new) == 2:
+                flag = 0
+                refPt.clear()
+                refPt.append([new[0][0], new[0][1]])
+                refPt.append([new[1][0], new[1][1]])
+                new.clear()
 
 
 if __name__ == "__main__":
-    m = 0
-    cap = cv2.VideoCapture(0)
-    cv2.namedWindow('img')
-    cv2.resizeWindow('img', 400, 150)
-    img = np.zeros((512, 100, 3), np.uint8)
-
-    cv2.createTrackbar('VarThreshold', 'img', 0, 300, trackbar_callback)
-    cv2.createTrackbar('Kernel', 'img', 0, 15, trackbar_callback)
-    cv2.createTrackbar('History', 'img', 0, 3000, trackbar_callback)
-    cv2.setTrackbarPos('VarThreshold', 'img', 16)
-    cv2.setTrackbarPos('History', 'img', 400)
-    fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=16, history=400)
-    print(fgbg)
     while 1:
-        _, frame = cap.read()
-        kernel = np.ones((cv2.getTrackbarPos('Kernel', 'img'), cv2.getTrackbarPos('Kernel', 'img')), np.float32) / 25
-        frame = morphologicalTransformations(m, frame, kernel)
-        frame = backgroundSubtraction(frame, fgbg)
-        cv2.imshow("frame", frame)
-        k = cv2.waitKey(1) & 0xff
-        if k == ord('w'):
-            m = 1
-        elif k == ord('x'):
-            m = 2
-        elif k == ord('y'):
-            m = 3
-        elif k == ord('z'):
-            m = 4
-        elif k == ord('a'):
-            m = 0
+        img = cv2.imread('picture.jpg')
+        if len(refPt) > 1:
+            img = foregroundExtraction(img)
+        cv2.imshow("pic", img)
+        cv2.setMouseCallback("pic", click_event)
+        k = cv2.waitKey(1)
         if k == 27:
             break
-cv2.destroyAllWindows()
-cap.release()
